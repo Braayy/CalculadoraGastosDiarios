1b7db61b54691b998a43e9837204133969794840
Braayy <brayangomesruel@gmail.com>
2022-12-01 22:58:17 -0300

    org: organized better the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Component, For, Show } from 'solid-js';
 import { createSignal } from 'solid-js';
+import { CalculatorForm, CalculatorFormData } from './components/CalculatorForm';
 import { Holiday } from './types';
 
-class AppResult {
-    public readonly value: number;
-    public readonly holidays: Holiday[];
-
-    constructor(value: number, holidays: Holiday[]) {
-        this.value = value;
-        this.holidays = holidays;
-    }
+class Calculation {
+    constructor(
+        public readonly startDate: string,
+        public readonly endDate: string,
+        public readonly value: number,
+        public readonly holidays: Holiday[]
+    ) {}
 }
 
 function getDatesBetween(startDate: Date, endDate: Date): Date[] {
@@ -35,20 +35,23 @@ function getDatesBetween(startDate: Date, endDate: Date): Date[] {
     return dates;
 }
 
+function letNotNull<T, V>(value: T | null, closure: (value: T) => V): V | null {
+    if (value != null) {
+        return closure(value);
+    }
+    return null;
+}
+
 const App: Component<{ holidays: Holiday[] }> = (props) => {
-    const [dailyValue, setDailyValue] = createSignal(0);
-    const [ignoredDays, setIgnoredDays] = createSignal(0);
-    const [startDate, setStartDate] = createSignal(new Date());
-    const [endDate, setEndDate] = createSignal(new Date());
-    const [result, setResult] = createSignal<AppResult | null>(null);
+    const [calculation, setCalculation] = createSignal<Calculation | null>(null);
 
-    function calculate(event: SubmitEvent) {
-        event.preventDefault();
-
+    function handleSubmit(data: CalculatorFormData) {
+        const { dailyValue, ignoredDays, startDate, endDate } = data;
+    
         let validDays = 0;
         let holidays = [];
-        for (const date of getDatesBetween(startDate(), endDate())) {
-            if (isIgnoredDaysBitSet(7)) {
+        for (const date of getDatesBetween(startDate, endDate)) {
+            if (isIgnoredDaysBitSet(ignoredDays, 7)) {
                 const holiday = getHolidayByDate(date, props.holidays);
                 if (holiday != null) {
                     holidays.push(holiday);
@@ -56,32 +59,23 @@ const App: Component<{ holidays: Holiday[] }> = (props) => {
                 }
             }
 
-            if (isIgnoredDaysBitSet(date.getDay())) {
+            if (isIgnoredDaysBitSet(ignoredDays, date.getDay())) {
                 continue;
             }
 
             validDays++;
         }
 
-        setResult(new AppResult(validDays * dailyValue(), holidays));
+        setCalculation(new Calculation(
+            formatDate(startDate),
+            formatDate(endDate),
+            dailyValue * validDays,
+            holidays,
+        ));
     }
 
-    function handleStartDateInput(event: InputEvent) {
-        const targetElement = event.target as HTMLInputElement;
-        setStartDate(new Date(Date.parse(targetElement.value.replaceAll('-', '/'))));
-    }
-
-    function handleEndDateInput(event: InputEvent) {
-        const targetElement = event.target as HTMLInputElement;
-        setEndDate(new Date(Date.parse(targetElement.value.replaceAll('-', '/'))));
-    }
-
-    function toggleIgnoredDaysBit(bitIndex: number) {
-        setIgnoredDays(ignoredDays() ^ (1 << bitIndex));
-    }
-
-    function isIgnoredDaysBitSet(bitIndex: number): boolean {
-        return (ignoredDays() & (1 << bitIndex)) > 0;
+    function isIgnoredDaysBitSet(ignoredDays: number, bitIndex: number): boolean {
+        return (ignoredDays & (1 << bitIndex)) > 0;
     }
 
     function getHolidayByDate(date: Date, holidays: Holiday[]): Holiday | null {
@@ -99,89 +93,29 @@ const App: Component<{ holidays: Holiday[] }> = (props) => {
 
     return (
         <div class="container min-vh-100 d-flex flex-column align-items-center">
-            <div class="">
+            <div>
                 <h1 class="text-center">Calculadora de Gastos Diários</h1>
             </div>
-            <form class="col-5 mb-5" onSubmit={calculate}>
-                <div class="mb-3 col-6">
-                    <div class="input-group">
-                        <span class="input-group-text">R$</span>
-                        <input type="number" step=".01" class="form-control" name="value" id="value" placeholder="Valor Diário" onInput={(event) => setDailyValue(parseFloat((event.target as HTMLInputElement).value))} />
+            <CalculatorForm onSubmit={handleSubmit} />
+            <Show when={calculation() != null}>
+                {letNotNull(calculation(), (calculation) =>
+                    <div>
+                        <p class="lead">O valor calculado para o periodo de <strong>{calculation.startDate}</strong> até <strong>{calculation.endDate}</strong> é de <strong>R${calculation.value.toFixed(2)}</strong></p>
+                        <Show when={calculation.holidays.length > 0}>
+                            <p>Foram encontrados os seguintes feriados durante o período selecionado:</p>
+                            <ul class="list-group col-6">
+                                <For each={calculation.holidays}>{(holiday) =>
+                                    <li class="list-group-item">{holiday.date.day.toString().padStart(2, '0')}/{holiday.date.month.toString().padStart(2, '0')} - {holiday.name}</li>
+                                }</For>
+                            </ul>
+                        </Show>
                     </div>
-                </div>
-                <div class="mb-3">
-                    <div class="form-text mb-1">Selecione os dias que não entrarão na conta:</div>
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" name="sunday" id="sunday" onInput={() => toggleIgnoredDaysBit(0)} />
-                        <label class="form-check-label" for="sunday">Domingo</label>
-                    </div>
-
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" name="monday" id="monday" onInput={() => toggleIgnoredDaysBit(1)} />
-                        <label class="form-check-label" for="monday">Segunda-Feira</label>
-                    </div>
-
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" name="tuesday" id="tuesday" onInput={() => toggleIgnoredDaysBit(2)} />
-                        <label class="form-check-label" for="tuesday">Terça-Feira</label>
-                    </div>
-
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" name="wednesday" id="wednesday" onInput={() => toggleIgnoredDaysBit(3)} />
-                        <label class="form-check-label" for="wednesday">Quarta-Feira</label>
-                    </div>
-
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" name="thursday" id="thursday" onInput={() => toggleIgnoredDaysBit(4)} />
-                        <label class="form-check-label" for="thursday">Quinta-Feira</label>
-                    </div>
-
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" name="friday" id="friday" onInput={() => toggleIgnoredDaysBit(5)} />
-                        <label class="form-check-label" for="friday">Sexta-Feira</label>
-                    </div>
-
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" name="saturday" id="saturday" onInput={() => toggleIgnoredDaysBit(6)} />
-                        <label class="form-check-label" for="saturday">Sábado</label>
-                    </div>
-
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" name="holidays" id="holidays" onInput={() => toggleIgnoredDaysBit(7)} />
-                        <label class="form-check-label" for="holidays">Feriados</label>
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <div class="col-6">
-                        <div class="input-group">
-                            <span class="input-group-text">Início</span>
-                            <input type="date" class="form-control" name="start-period" onInput={handleStartDateInput} />
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="input-group">
-                            <span class="input-group-text">Fim</span>
-                            <input type="date" class="form-control" name="end-period" onInput={handleEndDateInput} />
-                        </div>
-                    </div>
-                </div>
-                <div class="d-flex justify-content-center">
-                    <input type="submit" class="btn btn-lg btn-success" value="Calcular" />
-                </div>
-            </form>
-            <Show when={result() != null}>
-                <p class="lead">O valor calculado para o periodo de <strong>{formatDate(startDate())}</strong> até <strong>{formatDate(endDate())}</strong> é de <strong>R${result()?.value.toFixed(2)}</strong></p>
-                <Show when={(result()?.holidays?.length || 0) > 0}>
-                    <p>Foram encontrados os seguintes feriados:</p>
-                    <ul class="list-group">
-                        <For each={result()?.holidays}>{(holiday) =>
-                            <li class="list-group-item">{holiday.date.day}/{holiday.date.month} - {holiday.name}</li>
-                        }</For>
-                    </ul>
-                </Show>
+                )}
             </Show>
-            <a href="https://github.com/Braayy/CalculadoraGastosDiarios" target="_blank" class="link-primary">Código Fonte</a>
-            <p>* Os feriados utilizados são validos para o município de Niterói/RJ</p>
+            <div class="flex-grow-1 d-flex flex-column justify-content-end">
+                <a href="https://github.com/Braayy/CalculadoraGastosDiarios" target="_blank" class="link-primary text-center">Código Fonte</a>
+                <p>* Os feriados utilizados são validos para o município de Niterói/RJ</p>
+            </div>
         </div>
     );
 };
